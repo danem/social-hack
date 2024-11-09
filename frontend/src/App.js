6ded896f892016373405248552c31bcc3faddf24
lbarberiscanoni <hllbck7@gmail.com>
2024-11-09 14:37:58 -0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h1>Hello, JSX!</h1>
+        <p>This is a component using JSX syntax.</p>
+    </div>
+  );
+}
+
+export default App
