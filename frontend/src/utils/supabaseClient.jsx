65df99cda5c507508d88f9aeb61f95352519e356
Dane Mason <danem.mason@gmail.com>
2024-11-10 12:23:28 -0800

    link to client details

--- frontend/src/utils/supabaseClient.jsx
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(process.env.REACT_APP_SUPA_URL, process.env.REACT_APP_ANON_API_KEY);
-
-export default supabase;
